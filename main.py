655d7a32116d729e39c8096607d84178c9172908
PatrycjaP1 <patrycja-polentek4321@wp.pl>
2022-12-13 20:25:03 +0100

    zmiana kolorów

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import matplotlib.pyplot as plt
 
 sns.set_theme(style="whitegrid")
 bmi = pd.read_csv("bmi2")
-
-g = sns.relplot(
-    data = bmi,
-    hue="kategoria",
-    x="imie", y="bmi",
-    palette="dark",
-    alpha=.6,
-    height=7
+palette_dict = {x: "gray" for x in bmi.imie.unique()}
+palette_dict["wygłodzenie"] = "navy"
+palette_dict["wychudzenie"] = "blue"
+palette_dict["niedowaga"] = "palegreen"
+palette_dict["pożądana masa ciała"] = "green"
+palette_dict["nadwaga"] = "yellow"
+palette_dict["otyłość pierwszego stopnia"] = "orange"
+palette_dict["otyłość drugiego stopnia"] = "red"
+palette_dict["otyłość trzeciego stopnia"] = "darkred"
+g = sns.catplot(
+    data=bmi,
+x="imie", y="bmi", hue="kategoria", s=15, palette=palette_dict,
 )
+
 g.despine(left=True)
-g.set_axis_labels("Osoba","Wartość Bmi")
+g.set_axis_labels("Osoba", "Wartość Bmi")
 plt.show()
