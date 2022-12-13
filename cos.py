c7b7bb21d2dad6c6bc3fe6efd4d1d56596b5292d
PatrycjaP1 <patrycja-polentek4321@wp.pl>
2022-12-13 20:00:47 +0100

    zmiana kolorów

--- cos.py
+++ cos.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 sns.set_theme(style="whitegrid")
 bmi = pd.read_csv("bmi2")
-palette_dict = {x:"gray" for x in bmi.imie.unique()}
+palette_dict = {x: "gray" for x in bmi.imie.unique()}
 palette_dict["wygłodzenie"] = "navy"
 palette_dict["wychudzenie"] = "blue"
 palette_dict["niedowaga"] = "palegreen"
@@ -14,15 +14,10 @@ palette_dict["otyłość pierwszego stopnia"] = "orange"
 palette_dict["otyłość drugiego stopnia"] = "red"
 palette_dict["otyłość trzeciego stopnia"] = "darkred"
 g = sns.catplot(
-    data = bmi,
-hue = "kategoria",
-
-    x="imie", y="bmi",
-palette = palette_dict,
+    data=bmi,
+x="imie", y="bmi", hue="kategoria", s=15, palette=palette_dict,
 )
 
 g.despine(left=True)
-g.set_axis_labels("Osoba","Wartość Bmi")
+g.set_axis_labels("Osoba", "Wartość Bmi")
 plt.show()
-
-
